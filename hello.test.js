286d947cced1868d340c62bedaa1cee796392491
17-100 <sebisteri@outlook.com>
2021-10-16 18:26:24 +0200

    redacts .github/workflows

--- hello.test.js
+++ hello.test.js
@@ -3,12 +3,14 @@ const supertest = require('supertest');
 const { expect } = require('@jest/globals');
 const request = supertest(app);
 
-
-    test('blabla', async () => {
+describe("My siple CICD-pipeline", () => {
+    test('main page returns json with {message: Hello World! }', async () => {
         const response = await request.get('/')
         expect(response.status).toBe(200)
         expect(response.body.message).toBe('Hello World!')
        /*  done(); */
     })
+});
+    
 
 
