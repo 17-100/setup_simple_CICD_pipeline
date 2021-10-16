819b62099f7d4b47fb185f25d27e302005b223c0
17-100 <sebisteri@outlook.com>
2021-10-16 23:30:10 +0200

    adds new yml file

--- hello.test.js
+++ hello.test.js
@@ -4,12 +4,14 @@ const { expect } = require('@jest/globals');
 const request = supertest(app);
 
 describe("My siple CICD-pipeline", () => {
-    test('main page returns json with {message: Hello World! }', async () => {
-        const response = await request.get('/')
-        expect(response.status).toBe(200)
-        expect(response.body.message).toBe('Hello World!')
-       /*  done(); */
-    })
+        test('main page returns json with {message: Hello World! }', async () => {
+            const response = await request.get('/')
+            expect(response.status).toBe(200)
+            expect(response.body.message).toBe('Hello World!')
+           /*  done(); */
+        })
+   
+    
 });
     
 
